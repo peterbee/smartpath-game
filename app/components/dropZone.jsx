5476c7d21ffae5f910e9805aa29edc25dcb2589f
Annastasia Psitos <annastasia.psitos@gmail.com>
2023-10-09 12:01:33 -0600

    dropzone

---
+++ app/components/dropZone.jsx
@@ -0,0 +1,9 @@
+export default function DropZone({ title }) {
+  return (
+    <div className='border-[5px] border-black grow h-[477px] mx-[50px] my-8'>
+      <h1 className='text-center text-5xl tracking-wide font-bold mt-4'>
+        {title}
+      </h1>
+    </div>
+  );
+}
