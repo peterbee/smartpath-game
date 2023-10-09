6aaed30fc8c1c68a84e21f273e5f75b1550693e8
Annastasia Psitos <annastasia.psitos@gmail.com>
2023-10-09 14:54:03 -0600

    hover styles

--- app/components/dropZone.jsx
+++ app/components/dropZone.jsx
@@ -5,12 +5,15 @@ export default function DropZone({ title }) {
     id: 'droppable',
   });
 
-  const hoverStyle = 'border-dashed border-blue p-4';
+  const hoverStyle =
+    'outline-[5px] outline-dashed outline-blue outline-offset-8';
 
   return (
     <div
       ref={setNodeRef}
-      className='border-[5px] border-black grow h-[477px] mx-[50px] my-8'
+      className={`border-[5px] border-black grow h-[477px] mx-[50px] my-8 ${
+        isOver ?? hoverStyle
+      }`}
     >
       <h1 className='text-center text-5xl tracking-wide font-bold mt-4'>
         {title}
