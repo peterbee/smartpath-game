7c33941cc4d3ef1b922207188d13d60ff2528798
Annastasia Psitos <annastasia.psitos@gmail.com>
2023-10-22 14:50:48 -0600

    adjustments

--- app/dndPage.js
+++ app/dndPage.js
@@ -2,13 +2,11 @@
 
 import { useState } from 'react';
 
-import Image from 'next/image';
-
 import { DndContext } from '@dnd-kit/core';
 import { arrayMove, SortableContext } from '@dnd-kit/sortable';
 
-import Chocolate from '../public/images/chocolate.png';
-import Joel from '../public/images/Joel_cartwheel.png';
+import Chocolate from '../public/assets/Chocolate_narrow.png';
+import Joel from '../public/assets/Joel_narrow.png';
 import DropItem from './components/dropItem';
 import DropZone from './components/dropZone';
 
@@ -86,12 +84,7 @@ export default function DndPage() {
                       key={container.item.id}
                       id={container.item.id}
                       alt={container.item.alt}
-                    >
-                      <Image
-                        src={container.item.src}
-                        alt={container.item.alt}
-                      />
-                    </DropItem>
+                    />
                   )}
                 </DropZone>
               );
@@ -100,15 +93,32 @@ export default function DndPage() {
           <div className='flex'>
             {items &&
               items.map((img) => {
-                return (
-                  <DropItem key={img.id} id={img.id} alt={img.alt}>
-                    <Image src={img.src} alt={img.alt} />
-                  </DropItem>
-                );
+                return <DropItem key={img.id} img={img} />;
               })}
           </div>
         </SortableContext>
       </DndContext>
     </article>
   );
+}
+
+{
+  /* <Image
+                        src={container.item.src}
+                        alt={container.item.alt}
+                      />
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='16'
+                        height='16'
+                        fill='currentColor'
+                        class='bi bi-arrows-move'
+                        viewBox='0 0 16 16'
+                      >
+                        <path
+                          fill-rule='evenodd'
+                          d='M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z'
+                        />
+                      </svg>
+                    </DropItem> */
 }
