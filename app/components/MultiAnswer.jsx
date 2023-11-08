b2b3c719a405e0c3d8aa0ecf2d9609677745d49f
Annastasia Psitos <annastasia.psitos@gmail.com>
2023-11-08 16:44:58 -0700

    multi choice

--- app/components/MultiAnswer.jsx
+++ app/components/MultiAnswer.jsx
@@ -1,7 +1,9 @@
-export default function MultiAnswer({ answer }) {
-  console.log(answer);
+export default function MultiAnswer({ answer, inputSelection }) {
   return (
-    <button className='w-96 py-9 m-5 bg-black opacity-75 text-6xl text-white text-center'>
+    <button
+      className='w-96 py-9 m-5 bg-black opacity-75 text-4xl text-white text-center'
+      onClick={() => inputSelection(answer)}
+    >
       {answer.text}
     </button>
   );
--- app/multiChoicePage.js
+++ app/multiChoicePage.js
@@ -8,18 +8,37 @@ export default function MultiChoicePage() {
     { id: 1, text: 'Yes', correct: true },
     { id: 2, text: 'No', correct: false },
   ];
-
   const question = "Does Maynard have enough money for the ship's repairs?";
 
+  const [selected, setSelected] = useState(null);
+
+  function selectAnswer(answer) {
+    setSelected(answer.correct);
+  }
+
   return (
-    <div>
-      <div className='flex m-7'>
-        <p className='h-3/4 w-3/4'>shopping list image</p>
-        <h1 className='text-7xl text-left leading-relaxed'>{question}</h1>
+    <div className='w-4/5'>
+      <div className='m-7'>
+        {/* <p className='h-3/4 w-3/4'>shopping list image</p> */}
+        <h1 className='text-6xl text-left leading-relaxed'>{question}</h1>
+        {selected === true && (
+          <p className='transition-opacity h-3/4 w-3/4 mt-5 text-4xl text-center text-green-600'>
+            Correct!
+          </p>
+        )}
+        {selected === false && (
+          <p className='transition-opacity h-3/4 w-3/4 mt-5 text-4xl text-center text-red-600'>
+            Nope, try again!
+          </p>
+        )}
       </div>
       <div className='flex justify-around'>
         {answers.map((answer) => (
-          <MultiAnswer key={answer.id} answer={answer} />
+          <MultiAnswer
+            key={answer.id}
+            answer={answer}
+            inputSelection={selectAnswer}
+          />
         ))}
       </div>
     </div>
